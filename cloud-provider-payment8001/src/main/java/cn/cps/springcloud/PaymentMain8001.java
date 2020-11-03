bbc044d45bdd4889b6ffb8293517f8fbdd07b541
Cai Peishen <peishen.cai@foxmail.com>
2020-11-03 17:22:30 +0800

    初始化搭建springcloud2020父工程及cloud-provider-payment8001子模块

---
+++ cloud-provider-payment8001/src/main/java/cn/cps/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package cn.cps.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Cai Peishen
+ * @Date: 2020/11/3 16:18
+ * @Description:
+ */
+@SpringBootApplication
+public class PaymentMain8001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/cn/cps/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,29 @@
+package cn.cps.springcloud.service.impl;
+
+import cn.cps.springcloud.dao.PaymentDao;
+import cn.cps.springcloud.entities.Payment;
+import cn.cps.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 10:40
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
