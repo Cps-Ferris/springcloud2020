65eddf9a9a8e83d46e18d425a24a0f5ed8631316
Cai Peishen <peishen.cai@foxmail.com>
2020-11-09 10:35:20 +0800

    单机Eureka配置

--- cloud-consumer-order80/src/main/java/cn/cps/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/cn/cps/springcloud/OrderMain80.java
@@ -2,11 +2,13 @@ package cn.cps.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @auther zzyy
  * @create 2020-02-18 17:20
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderMain80
 {
--- cloud-eureka-server7001/src/main/java/cn/cps/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/cn/cps/springcloud/EurekaMain7001.java
@@ -2,18 +2,19 @@ package cn.cps.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author: Cai Peishen
- * @Date: 2020/11/3 16:18
+ * @Date: 2020/11/9 9:22
  * @Description:
  */
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class PaymentMain8001
-{
+public class EurekaMain7001 {
+
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
+
 }
