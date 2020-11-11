2e80c5a4511b8e77387d3caf43d7eef8756a7548
Cai Peishen <peishen.cai@foxmail.com>
2020-11-11 09:59:58 +0800

    Discover服务发现 每个服务都可以查看服务列表

--- cloud-provider-payment8002/src/main/java/cn/cps/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/cn/cps/springcloud/controller/PaymentController.java
@@ -4,10 +4,14 @@ import cn.cps.springcloud.entities.CommonResult;
 import cn.cps.springcloud.entities.Payment;
 import cn.cps.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Author: Cai Peishen
@@ -19,16 +23,18 @@ import javax.annotation.Resource;
 public class PaymentController
 {
 
-    @Resource
-    private PaymentService paymentService;
-
     @Value("${server.port}")
     private String serverPort;
 
+    @Resource
+    private PaymentService paymentService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(@RequestBody Payment payment)
-    {
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*****插入结果："+result);
 
@@ -41,15 +47,25 @@ public class PaymentController
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
-    {
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         Payment payment = paymentService.getPaymentById(id);
 
-        if(payment != null)
-        {
+        if(payment != null){
             return new CommonResult(200,"查询成功,serverPort:  "+serverPort,payment);
         }else{
             return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
+        }
+    }
+
+    @GetMapping("/payment/discover")
+    public void discover(){
+        List<String> services = discoveryClient.getServices();
+        for(String s : services){
+            log.info("服务名："+s);
+            List<ServiceInstance> instances = discoveryClient.getInstances(s);
+            for(ServiceInstance instance : instances){
+                log.info("实例名："+instance.getInstanceId()+"\tIP："+instance.getHost()+"\t端口："+instance.getPort()+"\tURL："+instance.getUri());
+            }
         }
     }
 
