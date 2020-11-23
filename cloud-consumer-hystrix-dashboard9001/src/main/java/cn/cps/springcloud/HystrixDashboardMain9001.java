ca730159e911586aaae5f223dcecef17f27976e7
Cai Peishen <peishen.cai@foxmail.com>
2020-11-23 12:44:13 +0800

    Hystrix仪表监控：HystrixDashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/cn/cps/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package cn.cps.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author: Cai Peishen
+ * @Date: 2020/11/23 18:32
+ * @Description: Hystrix监控仪表盘
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
--- cloud-provider-hystrix-payment8001/src/main/java/cn/cps/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/cn/cps/springcloud/PaymentHystrixMain8001.java
@@ -1,9 +1,14 @@
 package cn.cps.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @Author: Cai Peishen
@@ -17,6 +22,29 @@ public class PaymentHystrixMain8001
 {
     public static void main(String[] args) {
             SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+
+    // 也是Dashboard的坑 需要加上去
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /**
+     * Dashboard的坑
+     * 此配置是为了服务监控而配置，与服务容错本身无关，springcloud升级后的坑
+     * ServletRegistrationBean因为springboot的默认路径不是"/hystrix.stream"
+     * 只要在自己的项目里配置上下面的servlet就可以了
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
