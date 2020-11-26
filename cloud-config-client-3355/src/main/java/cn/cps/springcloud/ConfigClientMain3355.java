8cea6cd7a8706465d43d1703075bb4bc74c8f5d6
Cai Peishen <peishen.cai@foxmail.com>
2020-11-26 11:17:01 +0800

    Config客户端配置

---
+++ cloud-config-client-3355/src/main/java/cn/cps/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package cn.cps.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Cai Peishen
+ * @Date: 2020/11/26 9:22
+ * @Description: config配置中心客户端
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355
+{
+    public static void main(String[] args) {
+            SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
