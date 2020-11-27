11d3e3242d2d1c974c46c848824798168b1a2e62
Cai Peishen <peishen.cai@foxmail.com>
2020-11-27 10:28:11 +0800

    Config使用Bus订阅动态刷新

---
+++ cloud-config-client-3366/src/main/java/cn/cps/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package cn.cps.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Cai Peishen
+ * @Date: 2020/11/27 9:22
+ * @Description:
+ */
+@RefreshScope
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+
+}
