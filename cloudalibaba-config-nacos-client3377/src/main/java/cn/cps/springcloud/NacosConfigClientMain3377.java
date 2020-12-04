2568cb5891a0685eff08fb08045754b471d25cb6
Cai Peishen <peishen.cai@foxmail.com>
2020-12-04 11:10:01 +0800

    Nacos获取远程配置

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/cps/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package cn.cps.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: Cai Peishen
+ * @Date: 2020/12/4 11:18
+ * @Description: Nacos获取远程配置
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
