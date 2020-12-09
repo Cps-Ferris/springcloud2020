3a4dc913b1530e55dda601b646e05cbc58ca1a70
Cai Peishen <peishen.cai@foxmail.com>
2020-12-09 09:55:13 +0800

    Sentinel集成Ribbon初始化

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/cn/cps/springcloud/OrderNacosMain84.java
@@ -0,0 +1,18 @@
+package cn.cps.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: Cai Peishen
+ * @Date: 2020/12/9 09:18
+ * @Description:
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderNacosMain84 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain84.class, args);
+    }
+}
---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/cn/cps/springcloud/controller/CircleBreakerController.java
@@ -0,0 +1,41 @@
+package cn.cps.springcloud.controller;
+
+import cn.cps.springcloud.entities.CommonResult;
+import cn.cps.springcloud.entities.Payment;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: Cai Peishen
+ * @Date: 2020/12/9 09:18
+ * @Description:
+ */
+@Slf4j
+@RestController
+public class CircleBreakerController {
+    public static final String SERVICE_URL = "http://nacos-payment-provider";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/consumer/fallback/{id}")
+    @SentinelResource(value = "fallback") //没有配置，访问该请求，让sentinel生效(懒加载)
+    public CommonResult<Payment> fallback(@PathVariable Long id) {
+        CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/"+id, CommonResult.class,id);
+
+        if (id == 4) {
+            throw new IllegalArgumentException ("IllegalArgumentException,非法参数异常....");
+        }else if (result.getData() == null) {
+            throw new NullPointerException ("NullPointerException,该ID没有对应记录,空指针异常");
+        }
+
+        return result;
+    }
+
+}
